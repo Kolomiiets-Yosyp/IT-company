5b97d7d34b1baec223b51361af2464e2280f3f0a
yosyp <kolomiets204@gmail.com>
2026-01-22 06:50:15 +0100

    забрав вкладку /core/ та grid/ та about/. переніс /build/ на головний екран замість EXPERIMENTAL LAB.

--- tworix/website/admin.py
+++ tworix/website/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
-from .models import Project, Contact, Content
+from .models import Project, Contact, Content, ProjectMedia
+
+class ProjectMediaInline(admin.TabularInline):
+    model = ProjectMedia
+    extra = 2
+    fields = ('media_type', 'image', 'video_url', 'order')
+    verbose_name = "Додаткове медіа"
+    verbose_name_plural = "Додаткові медіа"
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
-    search_fields = ('title', 'description')
+    list_display = ('title', 'short_description', 'description')
+    search_fields = ('title', 'description', 'short_description')
+    fieldsets = (
+        ('Основна інформація', {
+            'fields': ('title', 'image')
+        }),
+        ('Описи', {
+            'fields': ('short_description', 'description'),
+            'description': 'short_description - для сторінки /lab/, description - для сторінки проекту'
+        }),
+        ('Додатково', {
+            'fields': ('details', 'link'),
+            'classes': ('collapse',)
+        }),
+    )
+    inlines = [ProjectMediaInline]
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
@@ -16,3 +37,4 @@ class ContentAdmin(admin.ModelAdmin):
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(Content, ContentAdmin)
+admin.site.register(ProjectMedia)
