31c0b7c1be8d2b13c80c774ffbf364f22b001d31
yosyp <kolomiets204@gmail.com>
2026-01-24 15:37:40 +0100

    took away the /core/ and grid/ and about/ tab. moved /build/ to main screen instead of EXPERIMENTAL LAB.

--- tworix/website/admin.py
+++ tworix/website/admin.py
@@ -1,30 +1,9 @@
 from django.contrib import admin
-from .models import Project, Contact, Content, ProjectMedia
-
-class ProjectMediaInline(admin.TabularInline):
-    model = ProjectMedia
-    extra = 2
-    fields = ('media_type', 'image', 'video_url', 'order')
-    verbose_name = "Додаткове медіа"
-    verbose_name_plural = "Додаткові медіа"
+from .models import Project, Contact, Content
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'short_description', 'description')
-    search_fields = ('title', 'description', 'short_description')
-    fieldsets = (
-        ('Основна інформація', {
-            'fields': ('title', 'image')
-        }),
-        ('Описи', {
-            'fields': ('short_description', 'description'),
-            'description': 'short_description - для сторінки /lab/, description - для сторінки проекту'
-        }),
-        ('Додатково', {
-            'fields': ('details', 'link'),
-            'classes': ('collapse',)
-        }),
-    )
-    inlines = [ProjectMediaInline]
+    list_display = ('title', 'description')
+    search_fields = ('title', 'description')
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
@@ -37,4 +16,3 @@ class ContentAdmin(admin.ModelAdmin):
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(Content, ContentAdmin)
-admin.site.register(ProjectMedia)
--- tworix/website/models.py
+++ tworix/website/models.py
@@ -2,32 +2,13 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    short_description = models.TextField(blank=True, default="", help_text="Короткий опис для відображення на сторінці /lab/")
-    description = models.TextField(help_text="Повний опис для відображення на сторінці проекту")
-    image = models.ImageField(upload_to='projects/', blank=True, null=True, help_text="Головне фото (візитка) для відображення на /lab/")
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
     details = models.TextField(blank=True)
     link = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
-
-class ProjectMedia(models.Model):
-    MEDIA_TYPE_CHOICES = [
-        ('image', 'Фото'),
-        ('video', 'Відео'),
-    ]
-    
-    project = models.ForeignKey(Project, related_name='media', on_delete=models.CASCADE)
-    media_type = models.CharField(max_length=10, choices=MEDIA_TYPE_CHOICES, default='image')
-    image = models.ImageField(upload_to='projects/media/', blank=True, null=True, help_text="Фото (якщо media_type = image)")
-    video_url = models.URLField(blank=True, null=True, help_text="URL відео (YouTube, Vimeo тощо) - якщо media_type = video")
-    order = models.IntegerField(default=0, help_text="Порядок відображення")
-
-    class Meta:
-        ordering = ['order', 'id']
-
-    def __str__(self):
-        return f"{self.project.title} - {self.get_media_type_display()} #{self.order}"
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
